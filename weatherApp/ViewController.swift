15ff3ef7fd3ef3d173c5beba2bd5c874bd55fd16
lru0409 <74223246+lru0409@users.noreply.github.com>
2022-04-02 22:48:07 +0900

    create project

--- weatherApp/ViewController.swift
+++ weatherApp/ViewController.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var apiKey: String {
+        get {
+            // 생성한 .plist 파일 경로 불러오기
+            guard let filePath = Bundle.main.path(forResource: "KeyList", ofType: "plist") else {
+                fatalError("Couldn't find file 'KeyList.plist'.")
+            }
+            
+            // .plist를 딕셔너리로 받아오기
+            let plist = NSDictionary(contentsOfFile: filePath)
+            
+            // 딕셔너리에서 값 찾기
+            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
+                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'KeyList.plist'.")
+            }
+            return value
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
